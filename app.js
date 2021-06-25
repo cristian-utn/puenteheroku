b65d83c771e9b6230c82f1f48d19c0190de93456
cristian-utn <cristianmultiutn@hotmail.com>
2021-06-25 11:02:46 -0300

    version para quitar flex

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ var access_token_que_reciba_por_fetch={};
 var refresh_token_que_reciba_por_fetch={};
 var todainfo={};
 var exce;
-var accesstoken='APP_USR-3831000572264914-062122-d997f4093008e0beecb3b608baecfd45-137472805';
+var accesstoken='APP_USR-3831000572264914-062219-94764f230ddb0f0e250039b7e245950f-137472805';
+
 function listar(accesstoken){ //////////////////////////////////////////////////"    9789507880223    "
     // https://developers.mercadolibre.com.ar/es_ar/items-y-busquedas
     // https://api.mercadolibre.com//items?ids=$ITEM_ID1,$ITEM_ID2&attributes=$ATTRIBUTE1,$ATTRIBUTE2,$ATTRIBUTE3
@@ -78,6 +79,11 @@ app.post('/enviotoken', function (req, res) {
     access_token_que_reciba_por_fetch=JSON.parse(auxtemporal).access_token;
     refresh_token_que_reciba_por_fetch=JSON.parse(auxtemporal).refresh_token;
 });
+app.post('/enviolistaml', function (req, res) {
+    var auxtemporal=req.headers.body;
+    var au=JSON.parse(listajson).listademl;
+    fs.writeFileSync('assets/texto.json',JSON.stringify(au),'utf-8');
+});
 app.get('/info', function (req, res) {
     res.send({
         code:code_que_reciba_si_lo_tiene_la_url_de_mi_pagina.code,
@@ -105,29 +111,135 @@ const fs=require('fs');
 console.log('algo IMPORTANTE');
 // var au=JSON.parse(listajson);
 // console.log(au);
-const exelajson = () =>{
-    const excel=XLSX.readFile('assets/Stocks - Librerías 09-06-2021.xlsx',{CellDates: true});
-    var nombrehoja=excel.SheetNames;
-    var datos=XLSX.utils.sheet_to_json(excel.Sheets[nombrehoja[0]]);
-    console.log(datos);
-    exce=datos;
-}
+// const exelajson = () =>{
+//     const excel=XLSX.readFile('assets/Stocks - Librerías 09-06-2021.xlsx',{CellDates: true});
+//     var nombrehoja=excel.SheetNames;
+//     var datos=XLSX.utils.sheet_to_json(excel.Sheets[nombrehoja[0]]);
+//     console.log(datos);
+//     exce=datos;
+// }
 // exelajson();
 // au.push('algo');
 // au.push('mas');
 // console.log(au);
-
-// fs.writeFileSync('assets/texto.json',JSON.stringify(au),'utf-8');
+var itemID;
+var diccionario={};
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+        break;
+        }
+    }
+}
+function listatotal(){
+    var seller_id=137472805; //lo saque de una publicacion
+    var pais_mercadolibre='MLA';
+    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&limit=100',{
+        method:'GET',
+        headers:{
+            'Authorization': 'Bearer '+accesstoken
+        }
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        // console.log(datos);///////////////////////////
+        itemID=datos.scroll_id;
+        // publicaciones.innerHTML=itemID;
+        for(var i=0;i<215;i++){
+            sleep(200);
+            listatotal2();
+        }
+    });
+}
+function listatotal2(){
+    var seller_id=137472805; //lo saque de una publicacion
+    var pais_mercadolibre='MLA';
+    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&limit=100&scroll_id='+itemID,{
+    // fetch('https://api.mercadolibre.com/questions/search?search_type=scan&item='+itemID,{
+        method:'GET',
+        headers:{
+            'Authorization': 'Bearer '+accesstoken
+        }
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        // console.log(datos);///////////////////////////
+        var len=datos.results;
+        for(var i=0;i<len.length;i++){
+            diccionario[datos.results[i]]=datos.results[i];
+        }
+        itemID=datos.scroll_id;
+        console.log(Object.keys(diccionario).length);
+    });
+}
+// listatotal();
+// console.log(diccionario);
 console.log('algo IMPORTANTE');
 
-// const exelajson = () =>{
-//     const excel=XLSX.readFile('assets/Stocks-09-06-2021.xlsx');
-//     var nombrehoja=excel.SheetNames;
-//     let datos=XLSX.utils.sheet_to_json(excel.Sheets[nombrehoja[0]],{cellDates: true});
-//     console.log(datos);
-// }
+const exelajson = () =>{
+    // const excel=XLSX.readFile('assets/Stocks-09-06-2021.xlsx');
+    const excel=XLSX.readFile('assets/wald0906.xlsx');
+    var nombrehoja=excel.SheetNames;
+    let datos=XLSX.utils.sheet_to_json(excel.Sheets[nombrehoja[0]],{cellDates: true});
+    // console.log(datos);
+    console.log(Object.keys(datos).length);
+    diccionario=datos;
+    
+}
 // exelajson();
+var diccionario2={};
+var diccionario3={};
+// console.log(diccionario[32895]);
+// console.log(diccionario[32896]);
+// if('ISBN' in diccionario[32888])console.log('isbn');
+// else console.log('no hay');
+// if('ISBN' in diccionario[32889])console.log('no encontro');
+// else console.log('no hay');
+// console.log(Object.keys(diccionario).length);
 
+// for(var i=0;i<32889;i++){
+//     // console.log(diccionario[i].ISBN);
+//     var isbn2=new String(diccionario[i].ISBN);
+//     var isbn2=isbn2.trim();
+//     var titulo2=new String(diccionario[i].Titulo);
+//     var titulo2=titulo2.trim();
+//     var autor2=new String(diccionario[i].Autor);
+//     var autor2=autor2.trim();
+//     var editorial2=new String(diccionario[i].Editorial);
+//     var editorial2=editorial2.trim();
+//     var tema2=new String(diccionario[i].Tema);
+//     var tema2=tema2.trim();
+//     var portada2=new String(diccionario[i].Portada);
+//     var portada2=portada2.trim();
+//     if((isbn2 in diccionario2)==false){
+//         diccionario2[isbn2]={
+//             titulo:titulo2,
+//             autor:autor2,
+//             editorial:editorial2,
+//             tema:tema2,
+//             stock:diccionario[i].Stock,
+//             pvp:diccionario[i].PVP,
+//             foto:portada2};
+//     }
+//     else{
+//         console.log('repetido');
+//         diccionario3[isbn2]={
+//             titulo:titulo2,
+//             autor:autor2,
+//             editorial:editorial2,
+//             tema:tema2,
+//             stock:diccionario[i].Stock,
+//             pvp:diccionario[i].PVP,
+//             foto:portada2
+//         };
+//     }
+// }
+// console.log(diccionario2);
+// fs.writeFileSync('assets/wald222.json',JSON.stringify(diccionario2),'utf-8');
+// string.replace(/ /g, "");
+
+// fs.writeFileSync('assets/wald333.json',JSON.stringify(diccionario3),'utf-8');
 // en excel
 // 35142    con marco arriba
 // 32889    sin marco arriba
--- assets/js.js
+++ assets/js.js
@@ -163,9 +163,9 @@ function pedirRefreshToken(){
 // const key_secreto='MXZwKgLJCq8EBHCbbiuV0yPP32Q2CoWu';
 
 // accesstoken='APP_USR-8513290730145598-061818-e0e082a1235a079fb5c4c8303c6773ae-244140036';
-accesstoken='APP_USR-3831000572264914-062219-94764f230ddb0f0e250039b7e245950f-137472805';
+accesstoken='APP_USR-3831000572264914-062513-95f7b0725660a47996f27eb1a73d0097-137472805';
 // refreshtoken='TG-60cbe39be584b80008f478ec-244140036';
-refreshtoken='TG-60d2340f27243300070263ff-137472805';
+refreshtoken='TG-60d3b82c531f2b000846bc61-137472805';
 
 // itempublicado='MLA860038719';
 // "MLA412445"      categoria   Libros, Revistas y ComicsLibrosAutoayudaSuperación personal
@@ -379,7 +379,7 @@ function listar(){ //////////////////////////////////////////////////"    978950
     // https://developers.mercadolibre.com.ar/es_ar/items-y-busquedas
     // https://api.mercadolibre.com//items?ids=$ITEM_ID1,$ITEM_ID2&attributes=$ATTRIBUTE1,$ATTRIBUTE2,$ATTRIBUTE3
     // var seller_id=244140036; //lo saque de una publicacion
-    var seller_id=137472805; //lo saque de una publicacion
+    var seller_id=137472805; //lo saque de una publicacion 
     var pais_mercadolibre='MLA';
     fetch('https://api.mercadolibre.com/sites/'+pais_mercadolibre+'/search?seller_id='+seller_id,{
         method:'GET',
@@ -410,7 +410,13 @@ function listar(){ //////////////////////////////////////////////////"    978950
 }
 
 function mostrar_1_producto(){//segun ML
-    itempublicado='MLA926158965';
+    // itempublicado='MLA926158965'; 
+    // itempublicado='MLA926867095'; // crisyea
+    // itempublicado='MLA865389265'; // desconocido
+    // itempublicado='MLA775832082'; // desconocido
+    // itempublicado='MLA926112518'; // desconocido
+    itempublicado='MLA926760346'; // desconocido
+
     fetch('https://api.mercadolibre.com/items/'+itempublicado,{
         method:'GET',
         headers:{
@@ -553,14 +559,16 @@ function variable(datoexcel){
     console.log(datoexcel);
 }
 var itemID;
-var scroll="YXBpY29yZS1pdGVtcy1zZWFyY2g=:ZHMtYXBpY29yZS1pdGVtcy0wNQ==:FGluY2x1ZGVfY29udGV4dF91dWlkDXF1ZXJ5QW5kRmV0Y2gBFGFUN3ROSG9CUUZ4ZGp1YmVUS2NVAAAAAA2Rk1IWUi1RTm1CdTdUaGV3MTF1SUg0elB1QQ==";
+var diccionario={};
+var listaparadescargar=[];
+var iterador=0;
+// var scroll="YXBpY29yZS1pdGVtcy1zZWFyY2g=:ZHMtYXBpY29yZS1pdGVtcy0wNQ==:FGluY2x1ZGVfY29udGV4dF91dWlkDXF1ZXJ5QW5kRmV0Y2gBFGFUN3ROSG9CUUZ4ZGp1YmVUS2NVAAAAAA2Rk1IWUi1RTm1CdTdUaGV3MTF1SUg0elB1QQ==";
 // 21562 PUBLICADOS EN MERCADO LIBRE
 var scroll="";
 function listatotal(){
-    var seller_id=137472805; //lo saque de una publicacion
+    var seller_id=137472805; //lo saque de una publicacion tulibroya
     var pais_mercadolibre='MLA';
-    // fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&limit=100',{
-    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan',{
+    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&limit=100',{
         method:'GET',
         headers:{
             'Authorization': 'Bearer '+accesstoken
@@ -569,16 +577,42 @@ function listatotal(){
     .then( response=>response.json())
     .then( datos => {
         console.log(datos);///////////////////////////
+        var len=datos.results;
+        for(var i=0;i<len.length;i++){
+            diccionario[datos.results[i]]=datos.results[i];
+        }
         itemID=datos.scroll_id;
         // publicaciones.innerHTML=itemID;
+        for(var i=0;i<217;i++){
+            sleep(200);
+            listatotal2();
+        }
     });
 }
+var vardeArchivotxt= new XMLHttpRequest();
+vardeArchivotxt.open("GET","listapublicados.json",false);
+vardeArchivotxt.send(null);
+var listacompleta=JSON.parse(vardeArchivotxt.responseText);// lista total de todos los publicados
 
+vardeArchivotxt.open("GET","listaskuwald.json",false);
+vardeArchivotxt.send(null);
+var listaskuwald=JSON.parse(vardeArchivotxt.responseText);
+
+vardeArchivotxt.open("GET","wald0906.json",false);
+vardeArchivotxt.send(null);
+var listawald=JSON.parse(vardeArchivotxt.responseText);
+
+vardeArchivotxt.open("GET","wald2306.json",false);
+vardeArchivotxt.send(null);
+var listawaldayer=JSON.parse(vardeArchivotxt.responseText);
+vardeArchivotxt.open("GET","wald2406.json",false);
+vardeArchivotxt.send(null);
+var listawaldhoy=JSON.parse(vardeArchivotxt.responseText);
 function listatotal2(){
     var seller_id=137472805; //lo saque de una publicacion
     var pais_mercadolibre='MLA';
+    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&limit=100&scroll_id='+itemID,{
     // fetch('https://api.mercadolibre.com/questions/search?search_type=scan&item='+itemID,{
-    fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&croll_id='+itemID,{
         method:'GET',
         headers:{
             'Authorization': 'Bearer '+accesstoken
@@ -586,24 +620,370 @@ function listatotal2(){
     })
     .then( response=>response.json())
     .then( datos => {
-        console.log(datos);///////////////////////////
+        // console.log(datos);///////////////////////////
+        // itemID=datos.scroll_id;
+        var len=datos.results;
+        for(var i=0;i<len.length;i++){
+            diccionario[datos.results[i]]=datos.results[i];
+        }
         itemID=datos.scroll_id;
+        console.log(Object.keys(diccionario).length);
     });
 }
-function listatotal3(){
-    var seller_id=137472805; //lo saque de una publicacion
-    var pais_mercadolibre='MLA';
-    // fetch('https://api.mercadolibre.com/users/'+seller_id+'/items/search?search_type=scan&croll_id='+itemID,{
-        fetch('https://api.mercadolibre.com/questions/search?search_type=scan&item='+itemID,{
+function mostra(){
+    console.log('dic diccionario para llenar');
+    console.log(Object.keys(diccionario).length);
+    console.log('dic listacompleta');
+    console.log(Object.keys(listacompleta).length);
+    console.log('dic listawald antes');
+    console.log(Object.keys(listawald).length);
+    console.log('dic listawald ayer');
+    console.log(Object.keys(listawaldayer).length);
+    console.log('dic listawald hoy');
+    console.log(Object.keys(listawaldhoy).length);
+    console.log('list listaparadescargar');
+    console.log(listaparadescargar.length);
+}
+function envia_no_sirve(){
+    fetch('/enviolistaml',{
+        method:'POST',
+        headers: { // cabeceras HTTP
+            body: JSON.stringify({
+                listademl:diccionario
+            }) // convertimos
+        }
+        // body:data // convertimos
+    });
+}
+function download(){
+    // var diccionario2=JSON.parse(diccionario);
+    var file=new Blob([JSON.stringify(listaparadescargar)], { type: "application/json"});
+    var a= document.createElement('a');
+    a.href=URL.createObjectURL(file);
+    a.download='datos.json';
+    a.click();
+}
+var contISBN=0;
+var contISBNnulo=0;
+function extrae1por1(itemml){
+    // var itemml=listakey[0];
+    // var itemml='MLA926115330';
+    fetch('https://api.mercadolibre.com/items/'+itemml,{
         method:'GET',
         headers:{
-            'Authorization': 'Bearer '+accesstoken
+            'Authorization': 'Bearer '+accesstoken,
+            'content-type':'application/x-www-form-urlencoded'
         }
     })
     .then( response=>response.json())
     .then( datos => {
-        console.log(datos);///////////////////////////
-        itemID=datos.scroll_id;
-    });
+        // console.log(datos);
+
+        var isbn2='';
+        var titulo2='';
+        var autor2='';
+        var editorial2='';
+        var tema2='';
+        var precio2='';
+        var stock2='';
+        var fotos2=[];
+        var category_id2='';
+        var idml2='';
+        var sku2='';
+        var atributotitulo2='';
+        var idioma2='';
+        var formato2='';
+        var max_edad2='';
+        var min_edad2='';
+        var condiciones2='';
+
+        titulo2=datos.title;
+        // console.log(datos.title);
+        // console.log(datos.price);
+        precio2=datos.price;
+        // console.log(datos.pictures);
+        fotos2=datos.pictures;
+        // console.log('cantidad de stok');
+        // console.log(datos.available_quantity);
+        stock2=datos.available_quantity;
+        // console.log('categoria ML');
+        category_id2=datos.category_id;
+        // console.log("cambiar a un ml o id");
+        idml2=datos.id;
+
+        var atributosaux=datos.attributes;
+        // console.log(atributosaux);
+        var boolean=false;
+        for(var i=0;i<atributosaux.length;i++){
+            // console.log(Object.keys(atributosaux[i]).length);
+            // console.log(Object.values(atributosaux[i]).length);
+            var key=Object.keys(atributosaux[i]);
+            var value=Object.values(atributosaux[i]);
+
+            if(value.includes("SKU")||key.includes('SKU')){
+                // console.log('ENCONTRE SKU');
+                // console.log(atributosaux[i]); // toda la fila del atributo
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                sku2=atributosaux[i].value_name;
+                // console.log('ese es su SKU');
+            }
+            if(value.includes('ISBN')||key.includes('ISBN')){
+                if(value.includes('GTIN')||key.includes('GTIN')){
+                    // console.log('si esta en iteracion GTIN y ISBN ');
+                    if(atributosaux[i].value_name!=null){
+                        boolean=true;
+                        console.log(atributosaux[i].value_name+'     '+contISBN);// tiene ISBN
+                        isbn2=atributosaux[i].value_name;
+                    }
+                }
+                // console.log(atributosaux[i]);
+                // console.log(i);
+            }
+            if(value.includes('BOOK_TITLE')||key.includes('BOOK_TITLE')){
+                // console.log('titulo');// 
+                // console.log(atributosaux[i].value_name);//
+                atributotitulo2=atributosaux[i].value_name;
+            }
+            if(value.includes('AUTHOR')||key.includes('AUTHOR')){
+                // console.log('autor');
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                autor2=atributosaux[i].value_name;
+            }
+            if(value.includes('LANGUAGE')||key.includes('LANGUAGE')){
+                // console.log('idioma');
+                // console.log(atributosaux[i].value_name);//
+                idioma2=atributosaux[i].value_name;
+            }
+            if(value.includes('PUBLISHER')||key.includes('PUBLISHER')){
+                // console.log('editorial');
+                // console.log(atributosaux[i].value_name);// 
+                editorial2=atributosaux[i].value_name;
+            }
+            if(value.includes('FORMAT')||key.includes('FORMAT')){
+                // console.log('formato papel');
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                formato2=atributosaux[i].value_name;
+            }
+            // console.log('aqui deberia ir GENERO DEL LIBRO');
+            if(value.includes('NARRATION_TYPE')||key.includes('NARRATION_TYPE')){
+                // console.log('tipo de narracion');
+                // console.log(atributosaux[i].value_name);// EN EXCEL TEMA = EN ML TIPO DE NARRACION
+                tema2=atributosaux[i].value_name;
+            }
+            if(value.includes('MAX_RECOMMENDED_AGE')||key.includes('MAX_RECOMMENDED_AGE')){
+                // console.log('MAXI');
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                max_edad2=atributosaux[i].value_name;
+            }
+            if(value.includes('MIN_RECOMMENDED_AGE')||key.includes('MIN_RECOMMENDED_AGE')){
+                // console.log('MINI');
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                min_edad2=atributosaux[i].value_name;
+            }
+            if(value.includes('ITEM_CONDITION')||key.includes('ITEM_CONDITION')){
+                // console.log('estado del producto');
+                // console.log(atributosaux[i].value_name);// tiene ISBN
+                condiciones2=atributosaux[i].value_name;
+            }
+        }
+        if(boolean){
+            contISBN++;
+        }
+        else{
+            contISBNnulo++;
+            // console.log('no tiene ISBN');
+        }
+        listaparadescargar.push(
+        {
+            isbn:isbn2,
+            titulo:titulo2,
+            autor:autor2,
+            editorial:editorial2,
+            tema:tema2,
+            precio:precio2,
+            stock:stock2,
+            fotos:fotos2,
+            categoria:category_id2,
+            idml:idml2,
+            sku:sku2,
+            atributotitulo:atributotitulo2,
+            idioma:idioma2,
+            formato:formato2,
+            maxedad:max_edad2,
+            minedad:min_edad2,
+            condiciones:condiciones2
+        });
+        // var atributos=datos.attributes={};
+    })
+    .then(console.log('listo para descargrse'));
 }
-function mostra(){console.log(itemID)}
+// de 50
+// 10
+// 20
+// 30
+// 40
+// 
+// https://www.mercadolibre.com.ar/publicaciones/MLA775832082/modificar?callback_url=https%3A%2F%2Fwww.mercadolibre.com.ar%2Fpublicaciones%2Flistado%3Ffilters%3DACTIVE%7CEXCLUSIVE_CHANNEL_NOPROXIMITY%7CEXCLUSIVE_CHANNEL_NOPROXIMITY%26search%3D%26sort%3D%26page%3D1
+// https://www.mercadolibre.com.ar/publicaciones/MLA926760346/modificar?callback_url=https%3A%2F%2Fwww.mercadolibre.com.ar%2Fpublicaciones%2Flistado%3Ffilters%3DACTIVE%7CEXCLUSIVE_CHANNEL_NOPROXIMITY%7CEXCLUSIVE_CHANNEL_NOPROXIMITY%26search%3D%26sort%3D%26page%3D1
+function revisartodo(){
+    var listakey=Object.keys(listacompleta);
+    console.log(listakey.length);
+    // console.log(listakey[0]);
+    for(var i=0;i<listakey.length;i++){
+            // console.log('es el:_'+i);
+            sleep(8);
+            extrae1por1(listakey[i]);
+    }
+}
+var contaglobal=0;
+function revisa2dic(){
+    var contnull=0;
+    var contvacio=0;
+    var conte=0;
+    // var contskunull=0;
+    // var contskuvacio=0;
+    // var contsku=0;
+    console.log(listacompleta.length); // lista total de todos los publicados
+    // console.log(listacompleta[0].idml);
+    for(var i=0;i<listacompleta.length;i++){
+        // console.log(listacompleta[i].isbn);
+        // if(listacompleta[i].isbn==null)contnull++;
+        // else if(listacompleta[i].isbn=='')contvacio++;
+        // else conte++;
+
+        // console.log(listacompleta[i].isbn);
+        // if(listacompleta[i].idml==null)contnull++;
+        if(listacompleta[i].sku==''){
+            contnull++;
+            listaparadescargar.push(listacompleta[i].idml);
+        }
+        
+        // else conte++;
+    }
+    fetch('https://api.mercadolibre.com/items/'+listaparadescargar[contaglobal],{//    https://developers.mercadolibre.com.ar/es_ar/producto-sincroniza-modifica-publicaciones
+        method:'PUT',
+        headers:{
+            'Authorization': 'Bearer '+accesstoken,
+            'accept': 'application/json',
+            'content-type':'application/json'
+        },
+        body:JSON.stringify({
+            attributes:[
+                {
+                    id: "SELLER_SKU",
+                    // value_name: "9789507880223" // SOLO ACEPTA NUMEROS PERO EN STRING
+                    value_name: "TLY" // SOLO ACEPTA NUMEROS PERO EN STRING
+                }                           
+            ]
+        })
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        console.log(datos);
+        contaglobal++;
+        console.log(contaglobal);
+        repite();
+    });
+    console.log("cont null "+contnull);
+    console.log(listaparadescargar.length);
+    // console.log(contvacio);
+    // console.log(conte);
+}
+function repite(){
+    fetch('https://api.mercadolibre.com/items/'+listaparadescargar[contaglobal],{//    https://developers.mercadolibre.com.ar/es_ar/producto-sincroniza-modifica-publicaciones
+    method:'PUT',
+    headers:{
+        'Authorization': 'Bearer '+accesstoken,
+        'accept': 'application/json',
+        'content-type':'application/json'
+    },
+    body:JSON.stringify({
+        attributes:[
+            {
+                id: "SELLER_SKU",
+                // value_name: "9789507880223" // SOLO ACEPTA NUMEROS PERO EN STRING
+                value_name: "TLY" // SOLO ACEPTA NUMEROS PERO EN STRING
+            }                           
+        ]
+    })
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        console.log(datos);
+        contaglobal++;
+        console.log(contaglobal);
+        if(contaglobal<listaparadescargar.length){
+            repite();
+            sleep(50);
+        }
+    });
+  
+  }
+function detectamayor(){
+    var conte=0;
+    var listakeys=Object.keys(listawald);
+    console.log(listakeys.length);
+    for(var i=0;i<listakeys.length;i++){
+        if(listawald[listakeys[i]].stock>3){
+            conte++;
+        }
+        
+    }
+    console.log(conte);
+    // console.log(listawald[listakeys[2]].stock);
+
+}
+
+function muestra3datos(){
+    var key1=Object.keys(listawald);
+    var key2=Object.keys(listawaldayer);
+    var key3=Object.keys(listawaldhoy);
+    console.log(Object.keys(listawald).length);
+    console.log(Object.keys(listawaldayer).length);
+    console.log(Object.keys(listawaldhoy).length);
+    console.log(key1[0]);
+    console.log(listawald[key1[0]].stock);
+    console.log(listawaldayer[key2[0]].titulo);
+    console.log(listawaldhoy[key3[0]].pvp);
+
+}
+function waldsinflex(){
+    var contnull=0;
+    console.log(listaskuwald.length); // 15596
+
+    fetch('https://api.mercadolibre.com/sites/MLA/shipping/selfservice/items/'+listaskuwald[contaglobal],{//    https://developers.mercadolibre.com.ar/es_ar/producto-sincroniza-modifica-publicaciones
+        method:'DELETE',
+        headers:{
+            'Authorization': 'Bearer '+accesstoken,
+        }
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        console.log(datos);
+        contaglobal++;
+        console.log(contaglobal);
+        repitesku();
+    });
+
+    console.log(contnull);
+}
+function repitesku(){
+    fetch('https://api.mercadolibre.com/sites/MLA/shipping/selfservice/items/'+listaskuwald[contaglobal],{//    https://developers.mercadolibre.com.ar/es_ar/producto-sincroniza-modifica-publicaciones
+    method:'DELETE',
+    headers:{
+        'Authorization': 'Bearer '+accesstoken,
+    }
+    })
+    .then( response=>response.json())
+    .then( datos => {
+        console.log(datos);
+        contaglobal++;
+        console.log(contaglobal);
+        if(contaglobal<listaskuwald.length){
+            sleep(35);
+            repitesku();
+            sleep(35);
+        }
+    });
+  
+  }
